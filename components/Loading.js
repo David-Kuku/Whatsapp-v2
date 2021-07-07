09fe335e01a1b0cf0f4b7463f4132c95ee1718c5
Kuku David <psalmsgus99@gmail.com>
2021-07-07 01:27:56 +0100

    Updating next.config.js

--- components/Loading.js
+++ components/Loading.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import {Circle} from 'better-react-spinkit'
+import Image from 'next/image'
 
 function Loading() {
     return (
         <center style={{display: "grid", placeItems: "center", height:"100vh"}}>
             <div>
-                <img
+                <Image
                 src = "http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
                 alt = ""
                 style ={{marginBottom: 10}}
                 height={200}
+                width = {200}
                 />
                 <Circle color="lightgreen" size={60}/>
             </div>
--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
+  images: {
+    domains: ['assets.stickpng.com'],
+  },
   reactStrictMode: true,
 }
